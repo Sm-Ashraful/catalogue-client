ec162d73ab9cec162a8ba22e40cf588c83213aea
Sm-Ashraful <smashraful.dev@gmail.com>
2024-05-29 16:29:38 +0600

    change url

--- urlConfig.js
+++ urlConfig.js
@@ -1,2 +1,2 @@
-export const API = `https://catalogue-server.vercel.app/api`;
+export const API = `https://catalogue-server-git-main-smashrafuls-projects.vercel.app/api`;
 // export const API = `http://localhost:5032/api`;
