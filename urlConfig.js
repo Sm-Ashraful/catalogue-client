4af7591475be14879e1fba39c94ff546c753d672
Sm Ashraful <smashraful.dev@gmail.com>
2024-05-30 16:29:16 +0600

    Update urlConfig.js

--- urlConfig.js
+++ urlConfig.js
@@ -1,2 +1,2 @@
-export const API = `https://catalogue-git-main-smashrafuls-projects.vercel.app/api`;
+export const API = `https://catalogue-plum.vercel.app/api`;
 // export const API = `http://localhost:5032/api`;
